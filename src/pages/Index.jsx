86c5771c9da7a31a21de16c555cfd8bab6c711bd
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-09-11 13:06:53 -0600

    Obtener los datos del loader con useLoaderData

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,8 +1,55 @@
+import { useLoaderData } from "react-router-dom";
 
+export function loader(){
+    const clientes = [
+        {
+            id: 1,
+            nombre: 'Leonardo',
+            telefono: 102013313,
+            email: "leonardo@leonardo.com",
+            empresa: 'Xenon Indrustrial'
+        },
+        {
+            id: 2,
+            nombre: 'Karen',
+            telefono: 138198313,
+            email: "karen@leonardo.com",
+            empresa: 'Xenon Indrustrial'
+        },
+        {
+            id: 3,
+            nombre: 'Josue',
+            telefono: 31983913,
+            email: "josue@leonardo.com",
+            empresa: 'Xenon Indrustrial'
+        },
+        {
+            id: 4,
+            nombre: 'Miguel',
+            telefono: 319381983,
+            email: "miguel@leonardo.com",
+            empresa: 'Xenon Indrustrial'
+        },
+        {
+            id: 5,
+            nombre: 'Pedro',
+            telefono: 1398198938,
+            email: "pedro@leonardo.com",
+            empresa: 'Xenon Indrustrial'
+        },
+    ];
+    return clientes;
+}
 
 const Index = () => {
+
+    const clientes = useLoaderData();
+
     return (
-        <div>Clientes</div>
+        <>
+            <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
+            <p className="mt-3">Administra tus Clientes</p>
+        </>
     )
 }
 
