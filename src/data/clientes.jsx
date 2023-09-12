5076e66bee1e57b1ab83c63d026a45a126e30ceb
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-09-12 13:52:35 -0600

    Conectando JSON-Server a nuestro Proyecto

---
+++ src/data/clientes.jsx
@@ -0,0 +1,10 @@
+
+export async function obtenerClientes(){
+
+    const respuesta = await fetch(import.meta.env.VITE_API_URL)
+
+    const resultado = await respuesta.json()
+
+    return resultado
+
+}
--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,45 +1,11 @@
 import { useLoaderData } from "react-router-dom";
 import Cliente from "../components/Cliente";
+import { obtenerClientes } from "../data/clientes";
 
 export function loader(){
-    const clientes = [
-        {
-            id: 1,
-            nombre: 'Leonardo',
-            telefono: 102013313,
-            email: "leonardo@leonardo.com",
-            empresa: 'Xenon Indrustrial'
-        },
-        {
-            id: 2,
-            nombre: 'Karen',
-            telefono: 138198313,
-            email: "karen@leonardo.com",
-            empresa: 'Xenon Indrustrial'
-        },
-        {
-            id: 3,
-            nombre: 'Josue',
-            telefono: 31983913,
-            email: "josue@leonardo.com",
-            empresa: 'Xenon Indrustrial'
-        },
-        {
-            id: 4,
-            nombre: 'Miguel',
-            telefono: 319381983,
-            email: "miguel@leonardo.com",
-            empresa: 'Xenon Indrustrial'
-        },
-        {
-            id: 5,
-            nombre: 'Pedro',
-            telefono: 1398198938,
-            email: "pedro@leonardo.com",
-            empresa: 'Xenon Indrustrial'
-        },
-    ];
-    return clientes;
+    const clientes = obtenerClientes()
+
+    return clientes
 }
 
 const Index = () => {
