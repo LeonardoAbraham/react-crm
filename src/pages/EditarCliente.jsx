1e9feac659893963483bdc8b7803bc0bc14f1980
Leonardo Alonzo <leonardo.a@cliker.mx>
2023-09-13 17:14:38 -0600

    Creando la acción para Editar el cliente

--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,6 +1,7 @@
-import { Form, useNavigate, useLoaderData } from "react-router-dom"
-import { obtenerCliente } from "../data/clientes"
+import { Form, useNavigate, useLoaderData, useActionData, redirect } from "react-router-dom"
+import { obtenerCliente, actualizarCliente } from "../data/clientes"
 import Formulario from "../components/Formulario"
+import Error from "../components/Error"
 
 export async function loader({params}){
     const cliente = await obtenerCliente(params.clienteId)
@@ -16,10 +17,41 @@ export async function loader({params}){
     return cliente
 }
 
+export async function action({request, params}){
+    const formData = await request.formData();
+
+    const datos = Object.fromEntries(formData);
+
+    const email = formData.get('email')
+
+    //Validacion
+    const errores = [];
+    if(Object.values(datos).includes('')){
+        errores.push('Todos los campos son abligatorios')
+    }
+
+    let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+    if(!regex.test(email)){
+        errores.push('El Email no es valido')
+    }
+
+    // Retornar datos si hay errores
+    if(Object.keys(errores).length){
+        return errores
+    }
+
+    //Actualizar el cliente
+    await actualizarCliente(params.clienteId, datos)
+
+    return redirect('/')
+}
+
 const EditarCliente = () => {
 
     const navigate = useNavigate();
     const cliente = useLoaderData();
+    const errores = useActionData();
 
     return (
         <>
@@ -37,7 +69,7 @@ const EditarCliente = () => {
 
             <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
 
-                {/* { errores?.length && errores.map((error, i) => <Error key={i}>{error}</Error> ) } */}
+                { errores?.length && errores.map((error, i) => <Error key={i}>{error}</Error> ) }
 
                 <Form
                     method="post"
@@ -49,7 +81,7 @@ const EditarCliente = () => {
                     <input 
                         type="submit"
                         className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
-                        value="Registrar Cliente"
+                        value="Guardar Cambios"
                     />
                 </Form>
                 
