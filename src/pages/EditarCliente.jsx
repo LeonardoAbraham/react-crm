bc59d6842c5e9dcd35a3f4c01c784adcd3ebaae0
Leonardo Abraham Alonzo Martinez <leonardo.a@cliker.mx>
2023-09-13 10:35:54 -0600

    Primeros pasos con editar cliente y useParams para recuperar el ID

---
+++ src/pages/EditarCliente.jsx
@@ -0,0 +1,13 @@
+export async function loader({params}){
+    console.log(params)
+
+    return null
+}
+
+const EditarCliente = () => {
+    return (
+        <div>EditarCliente</div>
+    )
+}
+
+export default EditarCliente
